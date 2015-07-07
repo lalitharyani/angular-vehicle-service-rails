08331bc2f3fba62a75575b6dfff7218d56f16d8f
Lalit Haryani <lalit.haryani@nagarro.com>
2015-07-07 13:15:16 +0200

    View Request & Other changes

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,48 +2,48 @@
 var app = angular.module( 'VehicleService', ['ngRoute'] );
 
 app.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.
-            when('/requestform', {
-                templateUrl: '/angular_views/request-form.html',
-                controller: 'VehicleCtrl'
-            }).
-            when('/viewRequest/:requestId', {
-                  templateUrl: 'angular_views/request-form.html',
-                  controller: 'VehicleCtrl'
-            }).
-            otherwise({
-                templateUrl: '/angular_views/home.html',
-                controller: 'HomeCtrl'
-            });
-    }]);
+  function($routeProvider) {
+    $routeProvider.
+        when('/requestform', {
+            templateUrl: '/angular_views/request-form.html',
+            controller: 'VehicleCtrl'
+        }).
+        when('/viewRequest/:requestId', {
+              templateUrl: 'angular_views/request-form.html',
+              controller: 'VehicleCtrl'
+        }).
+        otherwise({
+            templateUrl: '/angular_views/home.html',
+            controller: 'HomeCtrl'
+        });
+  }]);
 
 //define vehicle factory 
 app.factory('VehicleFactory', function () {
-    return [];    // Return an array
-})
+  return [];    // Return an array
+});
 
 
 
 ///Home Controller
 app.controller('HomeCtrl', ['$scope', 'VehicleFactory', function ($scope, VehicleFactory) {
 
-    VehicleFactory = [];
-    // For each item in local storage...
-    for( item in localStorage ) {
-        // Parse the json string and add it to VehicleFactory factory
-        var newItem = angular.fromJson( localStorage[item] );
-        VehicleFactory.push( newItem );
-    }
-    $scope.vehicleRequests = VehicleFactory;
+  VehicleFactory = [];
+  // For each item in local storage...
+  for( item in localStorage ) {
+    // Parse the json string and add it to VehicleFactory factory
+    var newItem = angular.fromJson( localStorage[item] );
+    VehicleFactory.push( newItem );
+  }
 
+  $scope.vehicleRequests = VehicleFactory;
 
-    // Delete  Request from system
-    $scope.deleteRequest = function(requestID, index) {
-        // Delete item from localStorage & vehicleRequests array
-        localStorage.removeItem( 'item' + requestID );
-        $scope.vehicleRequests.splice(index, 1);
-    }
+  // Delete  Request from system
+  $scope.deleteRequest = function(requestID, index) {
+    // Delete item from localStorage & vehicleRequests array
+    localStorage.removeItem( 'item' + requestID );
+    $scope.vehicleRequests.splice(index, 1);
+  }
 
 }]);
 
@@ -52,61 +52,55 @@ app.controller('HomeCtrl', ['$scope', 'VehicleFactory', function ($scope, Vehicl
 
 ///Vehicle Controller
 app.controller('VehicleCtrl', ['$scope', '$location', 'VehicleFactory', '$routeParams', function ($scope, $location, VehicleFactory, $routeParams) {
+  
+  $scope.vehicleRequests = VehicleFactory;  // Array that will hold all vehicleRequests      
+  $scope.failed = '';    // A message displayed if the form fails to submit
 
-    $scope.editable = true;
-    $scope.isDisabled = false;
-   if($routeParams.requestId){
-     $scope.editable = false;
-     $scope.isDisabled = true;
-   }
+  $scope.editable = true;
+  $scope.isDisabled = false;
+  $scope.requestData = "";
+
+  if($routeParams.requestId){
+    $scope.editable = false;
+    $scope.isDisabled = true;
+    
+    //get the selected request from localstorage
+    var requestData = localStorage.getItem("item"+$routeParams.requestId);
+    $scope.requestData = angular.fromJson(requestData)
+
+  }
 
    
-    // Submit new Request with values from the form fields, then reset values of the fields
-    $scope.addRequest= function() {   
+  // Submit new Request with values from the form fields, then reset values of the fields
+  $scope.addRequest= function() {   
 
-        $scope.vehicleRequests = VehicleFactory;  // Array that will hold all vehicleRequests      
-        $scope.failed = '';    // A message displayed if the form fails to submit
+    // If all required fields are complete 
+    if( !$scope.addVehicleform.$error.required ) { 
 
-        // If all required fields are complete 
-        if( !$scope.addVehicleform.$error.required ) { 
+      // Remove warning
+      $scope.failed = '';
 
-            // Remove warning
-            $scope.failed = '';
+      // Store contact data in an object         
+      var newRequest = $scope.requestData;
+      newRequest["id"] = localStorage.length+1;
 
-            // Store contact data in an object         
-            var newRequest = {
-                id: (localStorage.length+1),    // id is used to identify this property when being delete from  storage  
-                firstName: $scope.firstName,
-                lastName: $scope.lastName,
-                registrationNo: $scope.registrationNo,
-                mobileNo: $scope.mobileNo,
-                address: $scope.address,
-                pickupDate: $scope.pickupDate,
-                pickupDate: $scope.returnDate
-            };  
+      // Add Request object to localStorage as the value to a new property
+      localStorage.setItem( 'item' + (localStorage.length+1), JSON.stringify(newRequest) );
 
-            // Add Request object to localStorage as the value to a new property
-            localStorage.setItem( 'item' + (localStorage.length+1), JSON.stringify(newRequest) );
+      // Add new Request object to the model by adding it to the vehicleRequests array
+      $scope.vehicleRequests.push( newRequest );
 
-            // Add new Request object to the model by adding it to the vehicleRequests array
-            $scope.vehicleRequests.push( newRequest );
+      // Reset the inputs values for the form
+      $scope.requestData = {};
+      
+      $location.path('/');
 
-            // Reset the inputs values for the form
-            $scope.firstName = '';
-            $scope.lastName = '';
-            $scope.registrationNo = '';
-            $scope.mobileNo = '';
-            $scope.address = '';
-            $scope.pickupDate = '';
-            $scope.pickupDate = '';
-       
-          $location.path('/');
-        } else {
-            // Add warning
-            $scope.failed = 'All fields must be filled.';
-        }
-  
-    };
+    } else {
+        // Add warning
+        $scope.failed = 'All fields must be filled.';
+    }
+
+  };
 
 
 }]);
